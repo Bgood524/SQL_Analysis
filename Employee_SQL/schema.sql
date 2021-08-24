08fd5a2a0e8f052609ff2243f365c91ca71956d7
Bgood524 <bgood524@gmail.com>
2021-08-24 14:01:00 -0500

    created DBD, saved to txt file, created tables in Postgres

---
+++ Employee_SQL/schema.sql
@@ -0,0 +1,47 @@
+CREATE TABLE Departments(
+	dept_no VARCHAR NOT NULL,
+	dept_Name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE titles(
+	title_ID VARCHAR NOT NULL, 
+	title VARCHAR NOT NULL,
+	PRIMARY KEY (title_ID)
+);
+DROP TABLE titles;
+
+CREATE TABLE employees(
+	emp_no INTEGER NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles (title_ID)
+	);
+	
+CREATE TABLE Dept_emp(
+	emp_no INTEGER NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES Departments (dept_no)
+	);
+	
+CREATE TABLE Dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INTEGER NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES Departments (dept_no)
+);
+
+CREATE TABLE salaries(
+	emp_no INTEGER NOT NULL,
+	salary INTEGER NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
